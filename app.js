ac9400bababb9e40487188cc75080f05daf9290a
Honghao Zeng <nat@nat.moe>
2022-07-07 17:15:37 -0700

    oops

--- app.js
+++ app.js
@@ -104,7 +104,7 @@
         render(res);
     });
 
-    // xhr.open('GET', `https://status-api.apernet.io/status.json?v=${Date.now()}`);
-    xhr.open('GET', `/status.json?v=${Date.now()}`);
+    xhr.open('GET', `https://status-api.apernet.io/status.json?v=${Date.now()}`);
+    // xhr.open('GET', `/status.json?v=${Date.now()}`);
     xhr.send();
 })();
